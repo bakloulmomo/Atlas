e2e08271813fb94e6093b1d3f8beff9c3d1004b9
Momo <mohamedbakloul98@gmail.com>
2026-02-11 00:15:14 +0100

    Update API_BASE URL for ngrok deployment

--- config.js
+++ config.js
@@ -4,6 +4,7 @@
  * Per GitHub Pages + ngrok: metti qui il tuo URL ngrok (es. https://xxxx.ngrok-free.app)
  */
 window.ATLAS_CONFIG = {
-  API_BASE: "http://127.0.0.1:4040/inspect/http"
+  API_BASE: "https://feastful-nonbaronial-lenna.ngrok-free.dev"
 };
+
 
